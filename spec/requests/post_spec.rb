b8caa1bcac0bba633638b8cf6d1f7da509f82aba
OmniaGM <omnia.gamal1988@gmail.com>
2012-12-30 18:54:27 +0200

    test add new comment

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -13,7 +13,7 @@ describe "post" do
 		end
 		it "creats new post successfully" do
 			fill_in('Name', :with => 'Mohamed')
-			fill_in('Title', :with => 'tested comment')
+			fill_in('Title', :with => 'tested post')
 			fill_in('Content', :with => 'any plaaaaaaaaaaaaaaaa')
 			click_button('Create Post')
 			page.should have_content("Post was successfully created.")
@@ -32,6 +32,23 @@ describe "post" do
 			fill_in('Content', :with => 'any plaaaaaaaaaaaaaaaa')
 			click_button('Create Post')
 			page.should have_selector("h2", text: "1 error prohibited this post from being saved:")
+		end
+		it "creats new comment successfully" do
+			fill_in('Name', :with => 'Mohamed')
+			fill_in('Title', :with => 'tested post')
+			fill_in('Content', :with => 'any plaaaaaaaaaaaaaaaa')
+			click_button('Create Post')
+			page.should have_content("Post was successfully created.")
+			
+			within('#comments_form') do
+				fill_in('Commenter', :with => 'commeter1')
+				fill_in('Body', :with => 'any plaaaaaaaaaaaaaaaa comment')
+				click_button('Create Comment')
+				
+			end
+			within('#comments') do
+				page.has_content?('commenter1')
+			end
 		end
 	end
 end
